28e2033da9d315427165af94000908f8044805ec
aubreymoore <aubreymoore@guam.net>
2022-08-29 15:15:59 +1000

    first draft

---
+++ shrink-pdf.sh
@@ -0,0 +1,6 @@
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=CRB-FIDL-screen.pdf CRB-FIDL.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=CRB-FIDL-ebook.pdf CRB-FIDL.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=CRB-FIDL-prepress.pdf CRB-FIDL.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=CRB-FIDL-printer.pdf CRB-FIDL.pdf
+ls -lh CRB-FIDL*.pdf
+
